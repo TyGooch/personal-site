a2c1109fdcda9c8b047f43ebcb423eefd1e98163
Ty Gooch <gooch.ty@gmail.com>
2018-03-31 17:27:36 -0700

    implemented responsive nav

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import { withContext, getContext } from 'recompose'
 import PropTypes from 'prop-types'
 import { hot } from 'react-hot-loader'
 import styled, { injectGlobal } from 'styled-components'
-import Particles from 'react-particles-js';
-
+import Particles from 'react-particles-js'
 import Routes from 'react-static-routes'
+import {Collapse} from 'react-collapse';
 
 // import logoImage from './assets/personalLogoNew.svg'
-import logoImage from './assets/logo.svg'
+import logoImage from './assets/logo2.svg'
 import './app.css'
+import './bootstrap.min.css'
 
 // const particles = Particles.init({
 //   selector: 'canvas',
@@ -130,49 +131,70 @@ const AnimatedRoutes = getContext({
   />
 ))
 
-const App = () => (
-  <Router>
-      <div className="app-container">
-        <nav className="header">
-          <Link to="/" exact activeClassName="header-logo-active" className="header-logo"><img src={logoImage} alt="" /></Link>
-          <nav className="header-links">
-            <Prefetch path='/about'><Link to="/about" className = "header-link"  activeStyle={{width: 'inherit', height: 'inherit'}} ><span className='header-link-text'>ABOUT</span></Link></Prefetch>
-            <Prefetch path='/portfolio'><Link to="/portfolio" className = "header-link" activeStyle={{width: 'inherit', height: 'inherit'}} ><span className='header-link-text'>PORTFOLIO</span></Link></Prefetch>
-            <Prefetch path='/resume'><Link to="/resume" className = "header-link" activeStyle={{width: 'inherit', height: 'inherit'}} ><span className='header-link-text'>RESUME</span></Link></Prefetch>
-            <Prefetch path='/contact'><Link to="/contact" className = "header-link" activeStyle={{width: 'inherit', height: 'inherit'}} ><span className='header-link-text'>CONTACT</span></Link></Prefetch>
-            <button className='menu-icon'><i className='fas fa-bars'></i></button>
-          </nav>
-          <nav className="social-links">
-            <div onClick={() => window.open('https://github.com/tygooch', '_blank')} className='social-link'><i className='fab fa-github'></i></div>
-            <div onClick={() => window.open('https://linkedin.com/in/tygooch', '_blank')} className='social-link'><i className='fab fa-linkedin-in'></i></div>
-            <div onClick={() => window.open('https://facebook.com/tybradleygooch', '_blank')} className='social-link'><i className='fab fa-facebook-f'></i></div>
-          </nav>
-        </nav>
-        <Particles
-          className="particles"
-          canvasClassName="particles-canvas"
-          width='100vw'
-          height='100vh'
-          style={
-            {
-              width: '100vw',
-              height: '100vh',
-              position: 'fixed',
-              top: '0px',
-              left: '0px',
-              zIndex: -1,
-              opacity: 1
-            }
-          }
-          params ={
-            { "fps_limit": 60, "particles": { "number": { "value":((window.innerWidth/400)*30), "density": { "enable": false, "value_area": 400 } }, "color": { "value": "#000000" }, "shape": { "type":"circle" }, "opacity": { "value":0.5, "random":false, }, "size": { "value":1, "random":false }, "line_linked": { "enable": true, "distance": 224, "color": "#000000", "opacity": 0.5, "width": 0.32 }, "move": { "enable":true, "speed":0.75, "direction":"none", "random":true, "straight":false, "out_mode":"bounce", "bounce":true, "attract": { "enable": false, "rotateX": -100, "rotateY": -100 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": false, "mode": "repulse" }, "onclick": { "enable": false, "mode": "push" }, "resize":false }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.66 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } } , "retina_detect":true }
-          }
-          />
-        <div className="content">
-          <Routes component={AnimatedRoutes} />
-        </div>
-      </div>
-  </Router>
-)
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      menuOpened: window.innerWidth > 700 ? true : false,
+      isMobile: window.innerWidth <= 700 ? true : false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize.bind(this))
+  }
+
+  handleResize(){
+    this.setState({menuOpened: window.innerWidth > 700 ? true : false, isMobile: window.innerWidth <= 700 ? true : false})
+  }
+
+  render(){
+    return(
+      <Router>
+          <div className="app-container">
+            <nav className="header">
+              <Link to="/" exact activeClassName="header-logo-active" className="header-logo"><img src={logoImage} alt="" /></Link>
+              <nav className={`header-links ${this.state.isMobile ? 'mobile' : null}`} >
+                  <Prefetch path='/about'><Link to="/about" className = "header-link"  activeStyle={{width: 'auto', height: 'inherit'}} style={{marginRight: this.state.isMobile ? '0px' : '0px'}}><span className='header-link-text'>ABOUT</span></Link></Prefetch>
+                  <Prefetch path='/portfolio'><Link to="/portfolio" className = "header-link" activeStyle={{width: 'auto', height: 'inherit'}} style={{marginRight: this.state.isMobile ? '0px' : '0px'}}><span className='header-link-text'>PORTFOLIO</span></Link></Prefetch>
+                  <Prefetch path='/resume'><Link to="/resume" className = "header-link" activeStyle={{width: 'auto', height: 'inherit'}} style={{marginRight: this.state.isMobile ? '0px' : '0px'}}><span className='header-link-text'>RESUME</span></Link></Prefetch>
+                  <Prefetch path='/contact'><Link to="/contact" className = "header-link" activeStyle={{width: 'auto', height: 'inherit'}} style={{marginRight: this.state.isMobile ? '0px' : '0px'}}><span className='header-link-text'>CONTACT</span></Link></Prefetch>
+              </nav>
+              <div>
+                <nav className="social-links">
+                  <div onClick={() => window.open('https://github.com/tygooch', '_blank')} className='social-link'><i className='fab fa-github'></i></div>
+                  <div onClick={() => window.open('https://linkedin.com/in/tygooch', '_blank')} className='social-link'><i className='fab fa-linkedin-in'></i></div>
+                  <div onClick={() => window.open('https://facebook.com/tybradleygooch', '_blank')} className='social-link'><i className='fab fa-facebook-f'></i></div>
+                </nav>
+              </div>
+            </nav>
+            <Particles
+              className="particles"
+              canvasClassName="particles-canvas"
+              width='100vw'
+              height='100vh'
+              style={
+                {
+                  width: '100vw',
+                  height: '100vh',
+                  position: 'fixed',
+                  top: '0px',
+                  left: '0px',
+                  zIndex: -1,
+                  opacity: 1
+                }
+              }
+              params ={
+                { "fps_limit": 60, "particles": { "number": { "value":((window.innerWidth/400)*30), "density": { "enable": false, "value_area": 400 } }, "color": { "value": "#000000" }, "shape": { "type":"circle" }, "opacity": { "value":0.5, "random":false, }, "size": { "value":1, "random":false }, "line_linked": { "enable": true, "distance": 224, "color": "#000000", "opacity": 0.5, "width": 0.32 }, "move": { "enable":true, "speed":0.75, "direction":"none", "random":true, "straight":false, "out_mode":"bounce", "bounce":true, "attract": { "enable": false, "rotateX": -100, "rotateY": -100 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": false, "mode": "repulse" }, "onclick": { "enable": false, "mode": "push" }, "resize":false }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.66 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } } , "retina_detect":true }
+              }
+              />
+            <div className="content" style={this.state.isMobile ? {height: 'calc(100% - 100px)', top: '100px'} : null}>
+              <Routes component={AnimatedRoutes} />
+            </div>
+          </div>
+      </Router>
+    )
+  }
+}
 
 export default hot(module)(App)
--- static.config.js
+++ static.config.js
@@ -68,7 +68,7 @@ export default {
       return (
         <Html>
           <Head>
-            <meta title="Ty Gooch" />
+           <title>Ty Gooch</title>
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossOrigin="anonymous"/>
